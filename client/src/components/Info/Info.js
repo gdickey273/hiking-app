ac1ccc18ad2310d30479637a478db69849201e25
Daniel Sargent <dansargent025@gmail.com>
2020-11-14 13:30:46 -0500

    Added CSS/SCSS/, Created Components for home page items, initial work on Login Modal

---
+++ client/src/components/Info/Info.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Info.css';
+
+function Info(){
+  return (
+    <div class="info">
+      <div class="carousel" data-flickity>
+        <div class="carousel-cell"></div>
+        <div class="carousel-cell"></div>
+        <div class="carousel-cell"></div>
+        <div class="carousel-cell"></div>
+        <div class="carousel-cell"></div>
+      </div>
+    </div>
+  )
+};
+
+export default Info;
