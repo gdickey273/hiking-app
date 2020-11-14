e1f5f88f09367e87ac134b5c07112087960e50b5
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-11-14 10:27:37 -0500

    completed dotenv file, started backend api calls for openweather and google maps directions

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const trailRoutes = require("./trails");
+const weatherRoutes = require("./weatherAPI");
+const directionsRoutes = require("./directionsAPI");
 
 // Trail routes
 router.use("/trails", trailRoutes);
+router.use("/weather", weatherRoutes);
+router.use("/directions", directionsRoutes);
 
 module.exports = router;
