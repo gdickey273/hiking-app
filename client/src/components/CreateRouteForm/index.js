a3f96196b84959366f852e97c927adda380c70b7
gdickey273 <gdickey273@gmail.com>
2020-11-17 11:06:10 -0500

    created basic component file for CreateRouteForm

---
+++ client/src/components/CreateRouteForm/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import CreateRouteMap from "../CreateRouteMap";
+
+const CreateRouteForm = (prop) => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default CreateRouteForm;
