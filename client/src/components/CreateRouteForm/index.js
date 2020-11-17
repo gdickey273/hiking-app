670474fee3d22201bc5b01ae93cf828dbeae7356
gdickey273 <gdickey273@gmail.com>
2020-11-17 11:18:49 -0500

    added trailType radio buttons

--- client/src/components/CreateRouteForm/index.js
+++ client/src/components/CreateRouteForm/index.js
@@ -1,10 +1,31 @@
+import { use } from "passport";
 import React, { useState } from "react";
 import CreateRouteMap from "../CreateRouteMap";
 
 const CreateRouteForm = (prop) => {
+
+  const [newTrailObj, setNewTrailObj] = useState({});
+  const [currentMarker, setCurrentMarker] = useState("");
+
+  const handleTypeClick = (event) => {
+    setNewTrailObj({ ...newTrailObj, trailType: event.target.value });
+    console.log(event.target.value);
+  }
   return (
-    <>
-    </>
+    <div>
+
+      <form>
+        <div onClick={handleTypeClick}>
+          <input type="radio" name="trailType" value="loop" />
+          <label for="male">Loop</label><br />
+          <input type="radio" id="female" name="trailType" value="outAndBack" />
+          <label for="female">Out 'n Back</label><br />
+          <input type="radio" id="other" name="trailType" value="aToB" />
+          <label for="other">A to B</label>
+        </div>
+      </form>
+    </div>
+
   )
 }
 
