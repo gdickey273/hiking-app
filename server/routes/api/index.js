1e0558de844cd31fcd5fa2c7a32ec9f3b69b5402
gdickey273 <gdickey273@gmail.com>
2020-11-18 13:41:25 -0500

    added geocodeAPI.js to index

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -3,11 +3,13 @@ const trailRoutes = require("./trails");
 const weatherRoutes = require("./weatherAPI");
 const directionsRoutes = require("./directionsAPI");
 const mapSearchRoutes = require("./mapSearchAPI");
+const geocodeRoutes = require("./geocodeAPI");
 
 // Trail routes
 router.use("/trails", trailRoutes);
 router.use("/weather", weatherRoutes);
 router.use("/directions", directionsRoutes);
 router.use("/mapSearch", mapSearchRoutes);
+router.use("/geocode", geocodeRoutes);
 
 module.exports = router;
