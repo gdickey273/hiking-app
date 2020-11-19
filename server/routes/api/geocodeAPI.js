e28f8d79b1402b5aa6166f6394c41fc488cf0e29
gdickey273 <gdickey273@gmail.com>
2020-11-19 11:22:46 -0500

    grabbing city and state from params instead of body

--- server/routes/api/geocodeAPI.js
+++ server/routes/api/geocodeAPI.js
@@ -4,15 +4,16 @@ const fetch = require('node-fetch');
 const key = process.env.REACT_APP_GOOGLE_MAPS_API;
 
 
-router.get('/', async function(req, res) {
-  const city = req.body.city;
-  const state = req.body.state;
+router.get('/:city/:state', async function(req, res) {
+
+  const city = req.params.city;
+  const state = req.params.state;
   const url = `https://maps.googleapis.com/maps/api/geocode/json?components=locality:${city}|administrative_area:${state}&key=${key}`;
-  
 
   const call = await fetch(url);
   const data = await call.json();
   res.json(data.results[0].geometry.location);
+  
 })
 
 module.exports = router;
