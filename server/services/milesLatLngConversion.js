b1156bd549d3c3df6ffb1a3d8543d697c3041d8c
gdickey273 <gdickey273@gmail.com>
2020-11-19 17:57:55 -0500

    fixed object structure for module.exports

--- server/services/milesLatLngConversion.js
+++ server/services/milesLatLngConversion.js
@@ -3,11 +3,11 @@ const degreesToRadians = Math.PI / 180.0;
 const radiansToDegrees = 180.0 / Math.PI;
 
 module.exports = {
-  getLatBounds = function (lat, miles) {
+  getLatBounds : function(lat, miles) {
     let radius = (miles / earthRadius) * radiansToDegrees;
     return [lat - radius, lat + radius];
   },
-  getLngBounds = function (lng, miles) {
+  getLngBounds : function(lng, miles) {
     let radius = (miles / earthRadius) * radiansToDegrees;
     return [lng - radius, lng + radius];
   }
