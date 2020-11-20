cdbec6a3daf412f12c72f5c0a6f11c0bfecd3195
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-11-20 14:13:15 -0500

    started google maps api js for map with user route/directions --- still needs work

---
+++ client/src/components/UserTrailsMap/trailMap.js
@@ -0,0 +1,82 @@
+/*global google*/
+import React, { useEffect, useState } from "react";
+import {
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer
+} from "react-google-maps";
+
+function UserTrailsMap(props) {
+  console.log(props);
+  const [directions, setDirections] = useState(null);
+  const [origin, setOrigin] = useState(null);
+
+  useEffect(() => {
+    const directionsService = new google.maps.DirectionsService();
+
+    // const origin = { lat: 35.886116, lng: -78.960417 };
+    console.log(parseInt(props.origin.split(',')[0]));
+    const originLatitude = parseInt(props.origin.split(',')[0]);
+    const originLongitude = parseInt(props.origin.split(',')[1]);
+    const newOrigin = { lat: originLatitude, lng: originLongitude };
+    // const destination = { lat: 35.886116, lng: -78.960417 };
+    setOrigin(newOrigin)
+    const destLatitude = parseInt(props.destination.split(',')[0]);
+    const destLongitude = parseInt(props.destination.split(',')[1]);
+    const destination = { lat: destLatitude, lng: destLongitude };
+    const waypoints = [];
+
+    for (let i = 0; i < props.waypoints.length; i++) {
+      waypoints.push({
+        location: new google.maps.LatLng(props.waypoints[i])
+      })
+    }
+
+    console.log(`origin: ${origin}, destination: ${destination}, waypoints: ${waypoints}`);
+
+    directionsService.route(
+      {
+        origin: newOrigin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.WALKING,
+        waypoints: waypoints
+        // waypoints: [
+        //   {
+        //     location: new google.maps.LatLng(35.862905, -78.976748)
+        //   }
+        // ]
+      },
+      (result, status) => {
+        if (status === google.maps.DirectionsStatus.OK) {
+          console.log(result)
+          setDirections(result);
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      }
+    );
+  }, []);
+
+
+  const TrailMapComponent = withGoogleMap(props => (
+    <GoogleMap
+      // defaultCenter={{ lat: 35.886116, lng: -78.960417 }}
+      defaultCenter={origin}
+      defaultZoom={13}
+    >
+      <DirectionsRenderer
+        directions={directions}
+      />
+    </GoogleMap>
+  ));
+  return origin && (
+    <TrailMapComponent
+      containerElement={<div style={{ height: `500px`, width: "500px" }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+  );
+
+}
+
+export default UserTrailsMap;
+
