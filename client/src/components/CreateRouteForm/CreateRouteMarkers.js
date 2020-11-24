240e743ebc8d26d223f532fb800346d67df58e26
gdickey273 <gdickey273@gmail.com>
2020-11-24 09:42:53 -0500

    restructured CreateRouteForm and fixed Origin/Destination drag

---
+++ client/src/components/CreateRouteForm/CreateRouteMarkers.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import CreateRouteMap from "../CreateRouteMap";
+import extAPI from "../../utils/extAPI";
+import API from "../../utils/API";
+function CreateRouteMarkers(props) {
+  const { newTrailObj, setNewTrailObj, centerCoords, setCenterCoords } = props;
+  const [currentMarker, setCurrentMarker] = useState("");
+
+  
+  const handleRemoveWaypoint = (event) => {
+    event.preventDefault();
+    let arr = newTrailObj.waypoints;
+    arr.splice(event.target.dataset.index, 1);
+    setNewTrailObj({ ...newTrailObj, waypoints: arr });
+  }
+  return (
+    <>
+      <button name="setOrigin" onClick={event => { event.preventDefault(); setCurrentMarker("Origin") }}>Set Origin</button>
+      <label for="setOrigin">Origin: {newTrailObj.origin && `Lat: ${newTrailObj.origin.lat()}, Lng: ${newTrailObj.origin.lng()}`}</label><br />
+
+      {newTrailObj.trailType === "aToB"
+        ?
+        <>
+          <button name="setDestination" onClick={event => { event.preventDefault(); setCurrentMarker("Destination") }}>Set Destination</button>
+          <label for="setDestination">Destination: {newTrailObj.destination && `Lat: ${newTrailObj.destination.lat()}, Lng: ${newTrailObj.destination.lng()}`}</label><br />
+        </>
+        :
+        newTrailObj.origin &&
+        <>
+          <label for="setDestination">Destination: {`Lat: ${newTrailObj.origin.lat()}, Lng: ${newTrailObj.origin.lng()}`}</label><br />
+        </>
+      }
+    
+
+
+      <div className="trail-maker-waypoints">
+        {newTrailObj.waypoints.map((wp, i) => (
+          <>
+            <button for={`setWaypoint${i + 1}`} onClick={event => { event.preventDefault(); setCurrentMarker(`Waypoint${i + 1}`) }}>Set Waypoint</button>
+            <label for="setOrigin">Waypoint #{i + 1}: {`Lat: ${wp.lat()}, Lng: ${wp.lng()}`}</label><br />
+            <button data-index={i} onClick={handleRemoveWaypoint}>x</button><br />
+          </>
+        ))}
+
+        <button onClick={event => { event.preventDefault(); setCurrentMarker(`Waypoint${newTrailObj.waypoints.length + 1}`) }}>Set {newTrailObj.waypoints.length > 0 ? "Another Waypoint?" : "A Waypoint"}</button><br />
+      </div>
+
+      <CreateRouteMap className="map-container" centerCoords={centerCoords} newTrailObj={newTrailObj} setNewTrailObj={setNewTrailObj} currentMarker={currentMarker} setCurrentMarker={setCurrentMarker} />
+
+    </>);
+}
+
+export default CreateRouteMarkers;
--- client/src/components/CreateRouteMap/index.js
+++ client/src/components/CreateRouteMap/index.js
@@ -3,17 +3,19 @@ import { Map, InfoWindow, Marker, Polyline, GoogleApiWrapper } from "google-maps
 
 
 function CreateRouteMap(props) {
-  const { currentMarker, setCurrentMarker, newTrailObj, setNewTrailObj } = props;
+  const { currentMarker, setCurrentMarker, newTrailObj, setNewTrailObj, centerCoords } = props;
   const [currentLocation, setCurrentLocation] = useState({});
 
+
   useEffect(() => {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((data) => {
-        const lat = data.coords.latitude;
-        const lng = data.coords.longitude;
-        setCurrentLocation({lat, lng});
-      })
-    }
+    console.log(centerCoords);
+    // if(navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition((data) => {
+    //     const lat = data.coords.latitude;
+    //     const lng = data.coords.longitude;
+    //     setCurrentLocation({lat, lng});
+    //   })
+    // }
   })
 
   function handleMapClick(mapProps, map, event) {
@@ -39,12 +41,21 @@ function CreateRouteMap(props) {
     const la = marker.position.lat();
     const ln = marker.position.lng();
     const latLng = marker.position;
-  
-    const i = parseInt(marker.title);
-    const arr = newTrailObj.waypoints;
-    arr.splice(i, 1, y.latLng);
 
-    setNewTrailObj({...newTrailObj, waypoints: arr});
+    if (marker.title === "Origin" || marker.title === "Destination") {
+      let key = marker.title.toLowerCase();
+      console.log("marker.title------------------", marker.title);
+      console.log("marker.title.toLowercase()", marker.title.toLowerCase())
+
+      setNewTrailObj({ ...newTrailObj, [key]: y.latLng });
+    } else {
+      const i = parseInt(marker.title);
+      const arr = newTrailObj.waypoints;
+      arr.splice(i, 1, y.latLng);
+
+      setNewTrailObj({ ...newTrailObj, waypoints: arr });
+    }
+
   }
   const polyCoords = [];
   if (newTrailObj.origin && newTrailObj.waypoints.length > 0) {
@@ -53,7 +64,7 @@ function CreateRouteMap(props) {
   }
   if (polyCoords.length > 0) {
     if (newTrailObj.trailType === "aToB") {
-      if(newTrailObj.destination) {
+      if (newTrailObj.destination) {
         polyCoords.push(newTrailObj.destination);
       }
     } else if (newTrailObj.trailType) {
@@ -61,20 +72,22 @@ function CreateRouteMap(props) {
     }
   }
 
- 
+
   return (
     <Map
       google={props.google}
       zoom={10}
-      style={{ width: "100%", height: "100%" }}
+      style={{ width: "100vw", height: "100vh" }}
       streetViewControl={false}
       fullscreenControl={false}
       onClick={handleMapClick}
-      center={props.centerCoords || currentLocation}
+      initialCenter={centerCoords}
     >
       {newTrailObj.origin &&
         <Marker
           position={newTrailObj.origin}
+          draggable={true}
+          onDragend={handleMarkerDrag}
           title={"Origin"}
           strokeColor="#ffffff"
         />
@@ -84,15 +97,17 @@ function CreateRouteMap(props) {
           position={wp}
           draggable={true}
           onDragend={handleMarkerDrag}
-          title={""+i}
+          title={"" + i}
           key={i}
-        
+
         />
       ))}
 
       {newTrailObj.destination &&
         <Marker
           position={newTrailObj.destination}
+          draggable={true}
+          onDragend={handleMarkerDrag}
           title="Destination" />
       }
 
@@ -102,6 +117,7 @@ function CreateRouteMap(props) {
           strokeColor="#008000"
           strokeOpacity={0.8}
           strokeWeight={2}
+          onClick={(a, b, c) => console.log(a, b, c.latLng.lat(), c.latLng.lng())}
         />
       }
     </Map>
