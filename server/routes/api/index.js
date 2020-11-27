e83271abad2495bf2e1a6cf02727f320917409b2
gdickey273 <gdickey273@gmail.com>
2020-11-27 12:31:53 -0500

    added distance to api routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,6 +4,7 @@ const weatherRoutes = require("./weatherAPI");
 const mapSearchRoutes = require("./mapSearchAPI");
 const geocodeRoutes = require("./geocodeAPI");
 const googleKeyRoutes = require("./googleMapKey");
+const distanceRoutes = require("./distance")
 
 // Trail routes
 router.use("/trails", trailRoutes);
@@ -11,5 +12,6 @@ router.use("/weather", weatherRoutes);
 router.use("/mapSearch", mapSearchRoutes);
 router.use("/geocode", geocodeRoutes);
 router.use("/key", googleKeyRoutes);
+router.use("/distance", distanceRoutes);
 
 module.exports = router;
