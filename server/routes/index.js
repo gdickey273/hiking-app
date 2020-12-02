c3a471a22ed4a449153562b07349d8739561c205
gdickey273 <gdickey273@gmail.com>
2020-12-01 21:50:49 -0500

    heroku fix attempt #2

--- server/routes/index.js
+++ server/routes/index.js
@@ -14,7 +14,7 @@ router.use("/api", imageRoutes);
 
 // If no routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 module.exports = router;
