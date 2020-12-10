be4adbb79dfa7e0adc9cefe63d20589b59bc0405
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-12-10 09:50:15 -0500

    hard coded weather API key, removed get forecast button and formatted forecast cards

--- client/src/components/WeatherConditions/Conditions.js
+++ client/src/components/WeatherConditions/Conditions.js
@@ -4,20 +4,20 @@ import classes from './Conditions.module.css'
 let moment = require('moment');
 
 const conditions = (props) => {
-    
     return (
         <div className={classes.Wrapper} className="weather-conditions">
 
-            {props.error && <small className={classes.Small}>Please enter valid coordinates.</small>}
+            {/* {props.error && <small className={classes.Small}>Please enter valid coordinates.</small>}
 
-            {props.loading && <div className={classes.Loader} />}
+            {props.loading && <div className={classes.Loader} />} */}
 
             {props.responseObj.map((obj, i) => (
                 <div key={i} className="col-sm-2">
                     <div className="card forecast-day">
-                        <p className="card-title">{moment((obj.dt * 1000)).format('dddd')}</p>
-                        <p className="text-muted">{moment(obj.dt * 1000).format('MMMM Do, h:mm a')}</p>                        
-                        <p>Feels like: {Math.round(((obj.main.temp)))} °F</p>
+                    <img src={`http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`}></img>  
+                        <p className="card-title">{moment((obj.dt * 1000)).format('dddd')} {moment(obj.dt * 1000).format('MM/D/YY')}</p>
+                        {/* <p className="text-muted"></p>                   */}
+                        <p>{Math.round(((obj.main.temp)))}°F</p>
                             <div className="card-body">
                                 <p className="card-text">{obj.weather[0].description}</p>
                             </div>
